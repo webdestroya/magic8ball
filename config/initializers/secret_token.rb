095aae20c56936c7fb6cd2f3a45aa352b67bae46
Mitch Dempsey <mrdempsey@gmail.com>
2010-09-19 19:51:57 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EightBall::Application.config.secret_token = '1bafba4a97f69a8b5d05151a1930bc04fe1dc5fcfcebdf3f4c9926cf64801267050bc440ac8ae580bfc11b8270c8460a9dacd2c75684033b2ef80bc216eda6db'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+EightBall::Application.config.session_store :cookie_store, :key => '_eight_ball_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+# EightBall::Application.config.session_store :active_record_store
