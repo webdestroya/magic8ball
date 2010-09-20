027a9d5f7ef7c5ffdc09a100db5c313150830a42
Mitch Dempsey <mrdempsey@gmail.com>
2010-09-19 20:11:03 -0700

    Fixing up the readme, and some stats

--- config/routes.rb
+++ config/routes.rb
@@ -48,7 +48,10 @@ EightBall::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  
+  match 'shake' => "magic_ball#show"
+  
+  root :to => "magic_ball#show"
 
   # See how all your routes lay out with "rake routes"
 
